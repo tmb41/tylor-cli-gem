5df4309175822b8b39b5f7d20ce95f96120bde52
tmb41 <tylor.balson@gmail.com>
2016-06-13 18:03:52 -0400

    added to CLI, but stuck on argument error for menue

--- financial_data_pull/lib/cli.rb
+++ financial_data_pull/lib/cli.rb
@@ -1,14 +1,17 @@
-#require "nokogiri"
-#require "open-nuri"
+require "nokogiri"
+require "open-uri"
+require "pry"
 
 
 class FinancialDataPull::CLI
+  attr_accessor :ticker, :title, :prevclose, :open, :bid, :ask, :oneyeartarget, :beta, :nextearningdate, :daysrange, :fiftytwoweekrange, :volume, :avgvolume, :marketcap, :pettm, :epsttm, :divyield
 
   def call
     initialprint
     menue
-    #scrape
+    scrape
     printticker
+    again?
   end
 
   def initialprint
@@ -16,12 +19,13 @@ class FinancialDataPull::CLI
   end
 
   def menue
+    puts "Please select your desired option:"
     puts "1. GOOG - Alphabet, Inc."
     puts "2. TSLA - Tesla Motors, Inc."
     puts "3. AAPL - Apple, Inc."
     puts "4. FB - Facebook, Inc."
     puts "5. Type in ticker manually"
-    puts "6. exit"
+    puts "6. exit\n"
     input = gets.strip.downcase
 
     while input != "exit"
@@ -29,7 +33,7 @@ class FinancialDataPull::CLI
     case input
         when "1"
           puts "Google selected"
-          ticker = "goog"
+          @ticker = "goog"
           input = "exit"
         when "2"
           puts "Tesla slected"
@@ -56,37 +60,61 @@ class FinancialDataPull::CLI
 
   end
 
-  def scrape(ticker)
-    #doc = website(blahblah(ticker)blahblah)
+  def scrape
+    
+    search_url = "http://finance.yahoo.com/q?s=#{@ticker}&ql=1"
+    #binding.pry
+    website = Nokogiri::HTML(open(search_url))
+    
 
-    # 0 - 14
-    # self.get_page.css("td.yfnc_tabledata1")[0].text
-
-    # Title / Ticker
-    # self.get_page.css(".title h2").text
+    # 0 - 14 from table
+     @prevclose = website.css("td.yfnc_tabledata1")[0].text
+     @open = website.css("td.yfnc_tabledata1")[1].text
+     @bid = website.css("td.yfnc_tabledata1")[2].text
+     @ask = website.css("td.yfnc_tabledata1")[3].text
+     @oneyeartarget = website.css("td.yfnc_tabledata1")[4].text
+     @beta = website.css("td.yfnc_tabledata1")[5].text
+     @nextearningdate = website.css("td.yfnc_tabledata1")[6].text
+     @daysrange = website.css("td.yfnc_tabledata1")[7].text
+     @fiftytwoweekrange = website.css("td.yfnc_tabledata1")[8].text
+     @volume = website.css("td.yfnc_tabledata1")[9].text
+     @avgvolume = website.css("td.yfnc_tabledata1")[10].text
+     @marketcap = website.css("td.yfnc_tabledata1")[11].text
+     @pettm = website.css("td.yfnc_tabledata1")[12].text
+     @epsttm = website.css("td.yfnc_tabledata1")[13].text
+     @divyield = website.css("td.yfnc_tabledata1")[14].text
+    
+    #title
+     @title = website.css(".title h2").text
+     #binding.pry
   end
 
 
 
   def printticker
     puts "......................................."
-    puts " ~~Stock Name and Ticker~~"
-    puts " Prev Close:"              
-    puts " Open:"
-    puts " Bid:"
-    puts " Ask:"
-    puts " 1y Target Est:"
-    puts " Beta:"
-    puts " Next Earnings Date:"
-    puts " Day's Range:"
-    puts " 52wk Range:"
-    puts " Volume:"
-    puts " Avg Vol (3m):"
-    puts " Market Cap:"
-    puts " P/E (ttm):"
-    puts " EPS (ttm):"
-    puts " Div & Yield:"
-    puts "......................................."
+    puts " ~~#{title}~~"
+    puts " Prev Close: #{prevclose}"              
+    puts " Open: #{open}"
+    puts " Bid: #{bid}"
+    puts " Ask: #{ask}"
+    puts " 1y Target Est: #{oneyeartarget}"
+    puts " Beta: #{beta}"
+    puts " Next Earnings Date: #{nextearningdate}"
+    puts " Day's Range: #{daysrange}"
+    puts " 52wk Range: #{fiftytwoweekrange}"
+    puts " Volume: #{volume}"
+    puts " Avg Vol (3m): #{avgvolume}"
+    puts " Market Cap: #{marketcap}"
+    puts " P/E (ttm): #{pettm}"
+    puts " EPS (ttm): #{epsttm}"
+    puts " Div & Yield: #{divyield}"
+    puts ".......................................\n\n"
+  end
+
+  def again?
+    input = nil
+    menue
   end
 
 
